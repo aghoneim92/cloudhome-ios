83d36666f42c7eea89063451035332264512bbf3
Ahmed Ghoneim <ahmedghoneim92@gmail.com>
2022-03-24 17:22:04 +0200

    Initial Commit

---
+++ cloudhome/cloudhomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudhomeApp.swift
+//  cloudhome
+//
+//  Created by Ahmed Ghoneim on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct cloudhomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
